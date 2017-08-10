d81285ecfefa74e0c414178dbacd03bd8848f864
ferris <yupferris@gmail.com>
2017-08-10 19:52:32 +0200

    Spoof msx-dos
    So many existing demos will actually ship as msx-dos disks with the demo alongside MSXDOS.SYS and COMMAND.COM, with an AUTOEXEC.BAT to start the demo when msx-dos boots. The major advantage here is that the msx-dos environment has a full 64k of mem banked in while running, so the demo can just use the full address space and not worry about doing a RAM search/mappers/etc. It also allows for use of msx-dos routines.
    
    License-wise, this appears to be OK for msx-dos2, but it's unclear whether or not this is OK for msx-dos. This is a problem in my case, since msx-dos2 requires 128kb RAM to run, but my machine (sony hb-f1xd) only has 64kb RAM. I actually suspect this is the reason why many MSX2 demos require 128kb RAM to run.
    
    However, after looking at the boot process, I noticed a couple fun things:
     - It's not msx-dos that ensures RAM is visible. It's unclear which part of the system is responsible for this, but this environment is actually initialized _before_ msx-dos is loaded.
     - Given a disk with a default msx-dos bootsector, the msx machine will boot msx-dos by loading a file called MSXDOS.SYS into memory at 0x100, and then transferring execution to it.
    
    Here's the fun bit: nothing stops us from making our _own_ MSXDOS.SYS file, which can contain anything we want. Since there are multiple versions of msx-dos, there doesn't appear to be any checksums or hashes or anything for the file itself. So, if we produce our demo as a program called MSXDOS.SYS, the system will set up our full 64kb RAM visibility and load our demo automatically to a deterministic load addr, at which point we have full control of the system. And it doesn't even require a custom bootloader :) .
    
    So yeah, that's what this patch does. Pretty neat! The only downside is now we don't have access to msx-dos' file loading routines, but since those appear to rely on the machine's internal bios/disk rom's anyways, it shouldn't make much difference. This keeps us from having to play nice with any sort of OS as well, and get the full mem range to ourselves.

--- projects/test/test.asm
+++ projects/test/test.asm
@@ -1,41 +1,27 @@
-; Empty ASCII mapping will give us default and suppress warnings
+; Empty ASCII mapping will give us default 1:1 mapping and suppress warnings
 .asciitable
 .enda
 
-; Map each slot to the 4 mem pages in the msx2 mem map
-;  Note that here we're talking wla slots which are like mem windows, so they map pretty 1:1
-;  with 4 pages of the msx2 mem map. However this differs from what's usually referred to
-;  as slots when talking about msx memory, which are distinct groups of pages, much like
-;  groups of wla banks that get swapped, and portions of which are visible through wla slots.
+; Just treat mem as one big contiguous area starting at $100
+;  Basically max TOTAL_SIZE is going to be ~64kb minus $100 (eg. $fe00), but wla is going to output
+;  the entire bank even if we don't fill it, so it's best to keep TOTAL_SIZE small and increase it
+;  as needed to reduce load time for bytes we don't use.
+
+.define TOTAL_SIZE $0200
+
 .memorymap
-defaultslot 2
-slotsize $4000
-slot 0 $0000
-slot 1 $4000
-slot 2 $8000
-slot 3 $c000
+defaultslot 0
+slotsize TOTAL_SIZE
+slot 0 $0100
 .endme
 
 .rombankmap
 bankstotal 1
-banksize $4000
+banksize TOTAL_SIZE
 banks 1
 .endro
 
-.bank 0 slot 2
-
-header:
-    .db $fe ; ID byte
-    .dw start_addr
-    .dw end_addr
-    .dw entry
-
-    .define CHPUT $00a2
-
-start_addr:
 entry:
-    call write_msg
-
     ; Disable interrupts (otherwise our VDP I/O could get screwed due to internal index flip flops)
     di
 
@@ -61,23 +47,5 @@ main_loop:
 
     jr main_loop
 
-write_msg:
-    ld hl, msg
-write_msg_byte:
-    ld a, (hl)
-    or a
-    ret z
-    call CHPUT
-    inc hl
-    jr write_msg_byte
-
-msg:
-    .asc "Hello, world!"
-    .db $13, $10, $00
-
-    ; TODO: Proper RAM section for this? This should technically work just fine, but I'm not
-    ;  sure how well WLA handles mixed RAM/ROM or if there's a nicer way to do this.
 color_value:
     .db $00
-
-end_addr:
