58661e2c092702812e58a86a1cd1ecbe3df6c667
ferris <yupferris@gmail.com>
2017-11-04 12:13:17 +0100

    Add startup screen skip to test script
    This just unthrottles and throttles the emu after a certain amount of emulated time. 8 seconds might be a bit much, but appears to work quite well given the current test project setup, and I don't mind adjusting it later.

--- scripts/test.tcl
+++ scripts/test.tcl
@@ -2,3 +2,7 @@ set scale_algorithm simple
 set scale_factor 3
 set scanline 0
 set blur 0
+
+# Skip startup screens by un-throttling emulator for a short period of time
+set throttle off
+after time 8 {set throttle on}
